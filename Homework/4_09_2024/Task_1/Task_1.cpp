83134a7a627f788bc50445f5fe35c1f6f66c256c
Иван Горинов <lodimin345@gmail.com>
2024-10-07 11:07:48 +0300

    Добавьте файлы проекта.

---
+++ Homework/4_09_2024/Task_1/Task_1.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+class Fraction
+{
+public:
+    double numerator;
+    double denominator;
+
+    // Сложение
+    double sum()
+    {
+        return numerator + denominator;
+    }
+
+    // Вычитание
+    double subs()
+    {
+        return numerator - denominator;
+    }
+
+    // Умножение
+    double mult()
+    {
+        return numerator * denominator;
+    }
+
+    // Деление
+    double div()
+    {
+        if (denominator != 0)
+            return numerator / denominator;
+        else
+            std::cout << "Деление на ноль!";
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    Fraction frac;
+
+    std::cout << "Введите числитель: ";
+    std::cin >> frac.numerator;
+
+    std::cout << "Введите знаменатель: ";
+    std::cin >> frac.denominator;
+
+    std::cout << "Сумма: " << frac.sum() << std::endl;
+    std::cout << "Разность: " << frac.subs() << std::endl;
+    std::cout << "Произведение: " << frac.mult() << std::endl;
+    std::cout << "Частное: " << frac.div() << std::endl;
+
+    return 0;
+}
