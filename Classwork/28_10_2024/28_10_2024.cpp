8a3c47ac2ae75f387813d2931b5df9a62e7a13ed
Иван Горинов <lodimin345@gmail.com>
2024-10-28 20:31:52 +0300

    Классная работа 28.10.24

---
+++ Classwork/28_10_2024/28_10_2024.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+class Calculator {
+public:
+    double solve(double leftOperand, double rightOperand, char operation) {
+        if (operation == '+') {
+            return leftOperand + rightOperand;
+        } else if (operation == '-') {
+            return leftOperand - rightOperand;
+        } else if (operation == '*') {
+            return leftOperand * rightOperand;
+        } else if (operation == '/') {
+            return leftOperand / rightOperand;
+        } else {
+            throw "Invalid input operation";
+        }
+        return 0;
+    }
+};
+
+int main()
+{
+    try {
+        Calculator calculator;
+        double result = calculator.solve(5, 6, '/');
+        std::cout << result;
+        return 0;
+    }
+    catch (...) {
+        std::cout << "Error!";
+    }
+}
---
+++ Classwork/28_10_2024_1/28_10_2024_1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iterator>
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    unsigned size{ 0 };
+    typedef std::istreambuf_iterator<char> buf_iter;
+
+    std::ifstream file;
+    file.open("file.txt");
+    if (!file.is_open())
+        std::cout << "Файл не может быть открыт!";
+    else
+    {
+        for (buf_iter i(file), e; i != e; ++i)
+        {
+            char c = *i;
+            size++;
+        }
+
+        char* text = new char[size];
+        int h = 0;
+        for (buf_iter i(file), e; i != e; ++i)
+        {
+            char c = *i;
+            text[h] = c;
+            h++;
+        }
+
+        file.close();
+
+        for (int i = 0; i < size; i++) {
+            std::cout << text[i];
+        }
+    }
+    return 0;
+}
