17f1bbf19d465539912eea9378c250dbda2efab7
Иван Горинов <lodimin345@gmail.com>
2024-10-30 20:27:05 +0300

    Классная работа 30.10.24

--- Classwork/28_10_2024_1/28_10_2024_1.cpp
+++ Classwork/28_10_2024_1/28_10_2024_1.cpp
@@ -1,40 +1,61 @@
-﻿#include <iostream>
+﻿#include <windows.h>
+#include <iostream>
 #include <fstream>
 #include <vector>
-#include <iterator>
 
 int main()
 {
     setlocale(LC_ALL, "rus");
-    unsigned size{ 0 };
-    typedef std::istreambuf_iterator<char> buf_iter;
+    std::vector<char> text = {};
+    size_t size = 0;
+    int i = 0;
 
     std::ifstream file;
     file.open("file.txt");
+
     if (!file.is_open())
         std::cout << "Файл не может быть открыт!";
     else
     {
-        for (buf_iter i(file), e; i != e; ++i)
+        while (!file.eof())
         {
-            char c = *i;
-            size++;
-        }
-
-        char* text = new char[size];
-        int h = 0;
-        for (buf_iter i(file), e; i != e; ++i)
-        {
-            char c = *i;
-            text[h] = c;
-            h++;
+            std::vector<char> tempWord;
+            char c = file.get();
+            if (c == '-')
+            {
+                tempWord.push_back(c);
+                size++;
+                c = file.get();
+                if (c == '\n')
+                {
+                    tempWord.push_back(c);
+                    size++;
+                    c = file.get();
+                    if (c == '-'){}
+                    else
+                    {
+                        tempWord.push_back(c);
+                        size++;
+                    }
+                }
+                else
+                {
+                    i++;
+                    size++;
+                }
+            }
+            else
+            {
+                i++;
+                size++;
+            }
         }
 
         file.close();
-
-        for (int i = 0; i < size; i++) {
-            std::cout << text[i];
-        }
+    }
+    for (int j = 0; j < size; j++)
+    {
+        std::cout << tempWord[j]
     }
     return 0;
 }
