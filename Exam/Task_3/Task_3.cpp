c4b1636f730228576a7d298cc04bc5cf1ab3b3c4
Иван Горинов <lodimin345@gmail.com>
2024-12-15 18:27:13 +0300

    Exam

---
+++ Exam/Task_3/Task_3.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+class Wallet {
+public:
+    std::string name;
+    double money = 0;
+    void Deposit(double money_deposit) {
+        money += money_deposit;
+        std::cout << "Счёт карты " << name << " успешно пополнен на " << money_deposit << "руб\n";
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int choice;
+    double money_deposit;
+    Wallet first_debit_card, first_credit_card;
+
+    first_debit_card.name = "0001";
+    first_credit_card.name = "0002";
+    first_credit_card.Deposit(100000);
+    first_debit_card.Deposit(100);
+
+    std::cout << "Выберите карту:\n1 - Дебетовая карта " << first_debit_card.name << std::endl;
+    std::cin >> choice;
+    switch (choice) {
+    case 1:
+        std::cout << "\nДебетовая карта " << first_debit_card.name;
+        std::cout << "\nСчёт: " << first_debit_card.money << "руб";
+    }
+    return 0;
+}
