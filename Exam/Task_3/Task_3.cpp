9de8286e6acc7766330c33e279f7b43396cd64ba
Иван Горинов <lodimin345@gmail.com>
2024-12-16 18:26:11 +0300

    Exam

--- Exam/Task_3/Task_3.cpp
+++ Exam/Task_3/Task_3.cpp
@@ -4,30 +4,57 @@ class Wallet {
 public:
     std::string name;
     double money = 0;
-    void Deposit(double money_deposit) {
+    void Deposit() {
+        double money_deposit;
+        std::cout << "Введите на какую сумму вы хотите пополнить счёт:";
+        std::cin >> money_deposit;
         money += money_deposit;
         std::cout << "Счёт карты " << name << " успешно пополнен на " << money_deposit << "руб\n";
     }
 };
 
-int main()
-{
-    setlocale(LC_ALL, "rus");
-    int choice;
-    double money_deposit;
+void Menu(int choice) {
     Wallet first_debit_card, first_credit_card;
 
     first_debit_card.name = "0001";
     first_credit_card.name = "0002";
-    first_credit_card.Deposit(100000);
-    first_debit_card.Deposit(100);
+    double money_deposit;
 
-    std::cout << "Выберите карту:\n1 - Дебетовая карта " << first_debit_card.name << std::endl;
+    std::cout << "Выберите карту:\n"
+        << "1 - Дебетовая карта " << first_debit_card.name << std::endl
+        << "0 - Выход\n";
+    std::cout << "Ваше действие: ";
     std::cin >> choice;
-    switch (choice) {
-    case 1:
-        std::cout << "\nДебетовая карта " << first_debit_card.name;
-        std::cout << "\nСчёт: " << first_debit_card.money << "руб";
+    while (choice != 0) {
+        switch (choice) {
+        case 1:
+            std::cout << "\nДебетовая карта " << first_debit_card.name;
+            std::cout << "\nВыберите действие:\n"
+                << "1 - Пополнить счёт\n"
+                << "0 - Назад\n";
+            std::cout << "Ваше действие: ";
+            std::cin >> choice;
+            switch (choice) {
+            case 1:
+                first_debit_card.Deposit();
+            }
+            break;
+        default:
+            std::cout << "Error!";
+            break;
+        }
     }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int choice = 999;
+
+    
+    while (choice != 0) {
+        Menu(choice);
+    }
+
     return 0;
 }
